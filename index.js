6999b50c1138d26a12746a4b0d8e09d26ab1510f
yurism <mascarenhasyuri@hotmail.com>
2024-03-14 20:31:36 -0300

    [Add] input to be passed as argument

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { franc } from "franc";
 import langs from "langs";
 
-const text = "As investigações do assassinato da vereadora Marielle Franco (PSOL-RJ) e de seu motorista Anderson Gomes, mortos a tiros há 6 anos, chegaram ao Supremo Tribunal Federal (STF). ";
-const langCode = franc(text)
+const input = process.argv[2];
+const langCode = franc(input)
 const language = langs.where("3", langCode);
 
 console.log(language.name)
